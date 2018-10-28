cd8805f6a348d4d6dcc34a643a35681537ec485c
Aldranod <kurzydlo.b@gmail.com>
2018-10-28 13:14:26 +0100

    fix

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour {
+
+    int score = 0;
+
+    private void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        int numberGameSesions = FindObjectsOfType<GameSession>().Length;
+        if(numberGameSesions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+}
